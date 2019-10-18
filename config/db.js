d05a5b59bc4774a8f2f3f8673bbf601cb042ceb3
amaan nadeem <amaannadeem@outlook.com>
2019-10-18 15:13:56 -0700

    added mongodb environmnet variable

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const MONGO_URI = config.get("MONGO_URI");
+const MONGO_URI =  process.env.MONGO_URI || config.get("MONGO_URI");
 
 const connectDB = async () => {
   try {
